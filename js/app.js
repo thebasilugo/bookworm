efe6ee35b444020bf4ba8f2dd8f71c1b3f18d649
Basil Ugo <basilugo2@gmail.com>
2025-03-13 12:44:13 +0100

    class styles

--- js/app.js
+++ js/app.js
@@ -45,7 +45,7 @@ function renderHome() {
 					</div>
 			</div>
 			<div class="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6">
-					<div class="bg-gray-400 text-neutral-content p-6 rounded-lg shadow-md">
+					<div class="bg-white p-6 rounded-lg shadow-md">
 							<h3 class="text-xl font-semibold mb-4">Recently Added Books</h3>
 							<ul class="space-y-2">
 									${books
